eca487ca708cc89578ab5176bddd45383752698c
estefanyAzofeifa <estefanyazofeifa4@gmail.com>
2024-07-10 20:48:40 -0600

    Add files via upload

---
+++ sem8/PruebaClase8.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba.clase8;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class PruebaClase8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+      // primer ejercio esta subido en teams
+         /*
+      Del ejercicio 1, solicite un número que el usuario desee buscar en el arreglo.
+Realice un método que le indique al usuario en cuál posición del arreglo se encuentra el número.
+Valide si el usuario quiere buscar un número que no se encuentra en el arreglo.      */  
+    /*int cantidadRegistro = Integer.parseInt(JOptionPane.showInputDialog("dijite la de registro"));
+    int edad[] = new int [cantidadRegistro];
+    solicitoDatosVector(edad);
+    imprimDatosVector(edad);
+}
+    public static void solicitoDatosVector (int vector[])}
+    for*/
+       
+     
+    
+             
+       // solicito la informacion
+        /* int edad[] = new int[3];
+        for (int i = 0; i < edad.length; i++) {
+            edad[i] = Integer.parseInt(JOptionPane.showInputDialog("dijite la edad"));
+
+        }
+        //imprime la 
+        for (int i = 0; i < edad.length; i++) {
+            JOptionPane.showMessageDialog(null, "la edad " + (i + 1) +": "+ edad[i]);
+
+        }
+    }*/
+
+        
+        int cantidadRegistros = Integer.parseInt(JOptionPane.showInputDialog("dijite la cantida de registros"));
+        int edad [][]= new int [cantidadRegistros][cantidadRegistros];
+        solicitaDatos bn
+        
+    }
+}
---
+++ sem8/Semana8.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana8;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Semana8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       // areglos
+    int edad[] = new int[5];
+        edad[0]= 56;
+        edad[2]= 15;
+        System.out.println(edad[0]);
+        System.out.println(edad[1]);
+        System.out.println(edad[2]);
+        //Struing edad2[] =  new String[5];
+        
+        
+        
+        
+    }
+    
+}
